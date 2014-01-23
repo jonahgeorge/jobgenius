4ea04f5c72e1796fdf24b4a95af20282a55cff00
Jonah George <jonah@jonahgeorge.com>
2014-01-22 21:45:51 -0800

    Fixed article edit bug

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,104 +1,109 @@
 var pool = require('../database').pool;
 
 exports.index = function (req, res) {
-	pool.getConnection(function (err, conn) {
+    pool.getConnection(function (err, conn) {
 
-		conn.query("select * from C_ARTICLE as A left join C_USER as U ON A.uid = U.uid where A.published = 1", function (err, articles) {
-			if (err) console.log(err);
-			res.render('articles/index', {
-				title : "Articles",
-				articles : articles,
-				entity : req.session.entity
-			});
-		});
+        conn.query("select * from C_ARTICLE as A left join C_USER as U ON A.uid = U.uid where A.published = 1", function (err, articles) {
+            if (err) console.log(err);
+            res.render('articles/index', {
+                title : "Articles",
+                articles : articles,
+                entity : req.session.entity
+            });
+        });
 
-		conn.release();
-	});
+        conn.release();
+    });
 };
 
 exports.show = function (req, res) {
-	pool.getConnection(function (err, conn) {
-		conn.query("select * from C_ARTICLE where aid = ?", req.params.aid, function (err, article) {
-			
-			if (err) console.log(err);
-			res.render('articles/show', {
-				article : article[0],
-				entity : req.session.entity
-			});
+    pool.getConnection(function (err, conn) {
+        conn.query("select * from C_ARTICLE where aid = ?", req.params.aid, function (err, article) {
+            
+            if (err) console.log(err);
+            res.render('articles/show', {
+                article : article[0],
+                entity : req.session.entity
+            });
 
-		});
-		conn.release();
-	});
+        });
+        conn.release();
+    });
 };
 
 exports.form = function (req, res) {
-	res.render('articles/form', {
-		entity : req.session.entity
-	});
+    res.render('articles/form', {
+        entity : req.session.entity
+    });
 };
 
 exports.edit = function (req, res) {
-	pool.getConnection(function (err, conn) {
-		conn.query("select * from C_ARTICLE where aid = ?", req.params.aid, function (err, article) {
-			res.render('articles/form', {
-				article : article[0],
-				entity : req.session.entity
-			});
-		});
-	});
+    pool.getConnection(function (err, conn) {
+        conn.query("select * from C_ARTICLE where aid = ?", req.params.aid, function (err, article) {
+            res.render('articles/form', {
+                article : article[0],
+                entity : req.session.entity
+            });
+        });
+    });
 };
 
 exports.create = function (req, res) {
-	pool.getConnection(function (err, conn) {
+    pool.getConnection(function (err, conn) {
 
-		var article = {
-			title : req.body.title,
-			subtitle : req.body.subtitle,
-			body : req.body.body,
-			uid : req.session.entity.uid,
-			published : 1,
-			timestamp : new Date()
-		};
+        var article = {
+            title : req.body.title,
+            subtitle : req.body.subtitle,
+            body : req.body.body,
+            uid : req.session.entity.uid,
+            published : 1,
+            timestamp : new Date()
+        };
 
-		conn.query("insert into C_ARTICLE set ?", article, function (err) {
-			if (err) console.log(err);
-			res.redirect('/a');
-		});
+        conn.query("insert into C_ARTICLE set ?", article, function (err) {
+            if (err) console.log(err);
+            res.redirect('/a');
+        });
         
-		conn.release();
-	});
+        conn.release();
+    });
 };
 
 exports.update = function (req, res) {
-	pool.getConnection(function (err, conn) {
+  pool.getConnection(function (err, conn) {
 
-		var article = {
-			title : req.body.title,
-			subtitle : req.body.subtitle,
-			body : req.body.body,
-			timestamp : new Date()
-		};
+    var sql = "update C_ARTICLE set "
+            + "title = "+pool.escape(req.body.title)+", "
+            + "subtitle = "+pool.escape(req.body.subtitle)+", "
+            + "body = "+pool.escape(req.body.body)+", "
+            + "timestamp = "+pool.escape(new Date())+" "
+            + "where aid = " + pool.escape(req.params.aid) + ";"
 
-		conn.query("update C_ARTICLE set ? where aid = " + req.params.aid, article, function (err) {
-			if (err) console.log(err);
-			res.redirect( "/a/" + req.params.aid );
-		});
-	});
+    console.log(sql);
+
+    conn.query(sql, function (err) {
+      if (err) console.log(err);
+      res.redirect( "/a/" + req.params.aid );
+    });
+  });
 };
 
 exports.delete = function (req, res) {
-   	pool.getConnection(function (err, conn) {
-	    conn.query("select * from C_ARTICLE where aid = ?", req.params.aid, function (err, article) {
-	    	if (article.uid == req.session.entity.uid || req.session.entity.role == 0) {
-			    conn.query("update C_ARTICLE set published = 0 where aid = ? ", req.params.aid, function (err) {
-			        if (err) {
-			        	console.log(err);
-			        	res.json({ "status" : "Error" });
-			        }
-			        res.json({ "status" : "Success" });
-			    });
-			}
-		});
-		conn.release();
-	});
+  pool.getConnection(function (err, conn) {
+    
+    conn.query("select * from C_ARTICLE where aid = ?", req.params.aid, function (err, article) {
+      
+      if (article.uid == req.session.entity.uid || req.session.entity.role == 0) {
+
+        conn.query("update C_ARTICLE set published = 0 where aid = ? ", req.params.aid, function (err) {
+          if (err) {
+            console.log(err);
+            res.json({ "status" : "Error" });
+          }
+          res.json({ "status" : "Success" });
+        });
+      }
+    });
+    conn.release();
+  });
 };
