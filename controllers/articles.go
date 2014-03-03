48e2d2ffedc6a33e2111e60318ed837c1b3d4b46
jonahgeorge <jonah.george@icloud.com>
2014-03-03 13:44:56 -0800

    Fixed Go html/template for project

--- controllers/articles.go
+++ controllers/articles.go
@@ -3,51 +3,62 @@ package controllers
 import (
   "log"
   "net/http"
+  "html/template"
   "database/sql"
-  // "github.com/hoisie/mustache"
-  "github.com/martini-contrib/render"
   . "github.com/jonahgeorge/husker/models"
   _ "github.com/Go-SQL-Driver/MySQL"
-  )
+)
+
+var t *template.Template
 
 type ArticleController struct {}
 
-func (a ArticleController) Index(ren render.Render, db *sql.DB) {
-  articles, err := ArticleModel{}.RetrieveAll(db) 
-  if err != nil {
-    log.Fatal(err)
-  }
-
-  data := struct {
-    Title    string
-    Articles []ArticleModel
-  }{
-    "Articles",
-    articles,
-  }
-
-  ren.HTML(200, "articles/index", data)
+func init() {
+  t = template.Must(template.ParseGlob("templates/articles/*"))
+  t = template.Must(t.ParseGlob("templates/shared/*"))
 }
 
-func (a ArticleController) Retrieve(ren render.Render, db *sql.DB, req *http.Request) {
-  article, err := ArticleModel{}.RetrieveOne(db, req.FormValue("q")) 
-  if err != nil {
-    log.Fatal(err)
-  }
+func (a ArticleController) Index(db *sql.DB) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    articles, err := ArticleModel{}.RetrieveAll(db) 
+    if err != nil {
+      log.Fatal(err)
+    }
 
-  data := struct {
-    Title    string
-    Article   ArticleModel
-  }{
-    article.Title,
-    article,
-  }
+    data := struct {
+      Title    string
+      Articles []ArticleModel
+    }{
+      "Articles",
+      articles,
+    }
 
-  //return mustache.RenderFile("views/articles/show.mustache", data)
-  ren.HTML(200, "articles/show", data)
+    t.ExecuteTemplate(w, "index", data) 
+  }
 }
 
-func (a ArticleController) Form(db *sql.DB) string {
-  return ""
+func (a ArticleController) Retrieve(db *sql.DB) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    article, err := ArticleModel{}.RetrieveOne(db, r.FormValue("q")) 
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    data := struct {
+      Title    string
+      Article   ArticleModel
+    }{
+      article.Title,
+      article,
+    }
+
+    t.ExecuteTemplate(w, "show", data) 
+  }
+}
+
+func (a ArticleController) Form(db *sql.DB) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    t.ExecuteTemplate(w, "form", nil) 
+  }
 }
 
