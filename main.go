1e9c7df3a35fe00bc4313a838921eeb81a4d543e
jonahgeorge <jonah.george@icloud.com>
2014-03-06 11:35:23 -0800

    Added support to serve static content

--- main.go
+++ main.go
@@ -1,34 +1,46 @@
 package main
 
 import (
-  "os"
-  "log"
-  "net/http"
-  "database/sql"
-  "github.com/gosexy/yaml"
-  "github.com/gosexy/to"
-  _ "github.com/Go-SQL-Driver/MySQL"
-  . "github.com/jonahgeorge/husker/controllers"
+	"database/sql"
+	_ "github.com/Go-SQL-Driver/MySQL"
+	"github.com/gosexy/to"
+	"github.com/gosexy/yaml"
+	. "github.com/jonahgeorge/husker/controllers"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-  conf, _ := yaml.Open("settings.yaml")
-  user := to.String(conf.Get("database", "user"))
-  pass := to.String(conf.Get("database", "pass"))
-  name := to.String(conf.Get("database", "name"))
+	conf, _ := yaml.Open("settings.yaml")
+	user := to.String(conf.Get("database", "user"))
+	pass := to.String(conf.Get("database", "pass"))
+	name := to.String(conf.Get("database", "name"))
 
-  db, err := sql.Open("mysql", user + ":" + pass + "@/" + name)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer db.Close()
-  db.SetMaxIdleConns(100)
+	db, err := sql.Open("mysql", user+":"+pass+"@/"+name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	db.SetMaxIdleConns(100)
 
-  // routes
-  http.HandleFunc("/articles", ArticleController{}.Index(db))
-  http.HandleFunc("/article", ArticleController{}.Retrieve(db))
+	// routes
+	http.HandleFunc("/articles", ArticleController{}.Index(db))
+	http.HandleFunc("/article", ArticleController{}.Retrieve(db))
 
-  // listen on environmetn port
-  http.ListenAndServe(":" + os.Getenv("PORT"), nil)
-  // http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/interviews", InterviewController{}.Index(db))
+	http.HandleFunc("/interview", InterviewController{}.Retrieve(db))
+
+    http.HandleFunc("/users", UserController{}.Index(db))
+
+	http.HandleFunc("/about", StaticController{}.About())
+	http.HandleFunc("/terms", StaticController{}.Terms())
+	http.HandleFunc("/privacy", StaticController{}.Privacy())
+
+    // serve static content
+    http.Handle("/resources/", http.StripPrefix("/resources/", http.FileServer(http.Dir("resources")))) 
+
+	// listen on env port
+	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	// http.ListenAndServe(":3000", nil)
 }
