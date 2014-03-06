07fda0ce4f08bab6b669b54869cfe9b51f8abb9c
jonahgeorge <jonah.george@icloud.com>
2014-03-06 11:39:28 -0800

    Minor changes

--- controllers/articles.go
+++ controllers/articles.go
@@ -1,64 +1,58 @@
 package controllers
 
 import (
-  "log"
-  "net/http"
-  "html/template"
-  "database/sql"
-  . "github.com/jonahgeorge/husker/models"
-  _ "github.com/Go-SQL-Driver/MySQL"
+	"database/sql"
+	_ "github.com/Go-SQL-Driver/MySQL"
+	. "github.com/jonahgeorge/husker/models"
+	"log"
+	"net/http"
 )
 
-var t *template.Template
-
-type ArticleController struct {}
-
-func init() {
-  t = template.Must(template.ParseGlob("templates/articles/*"))
-  t = template.Must(t.ParseGlob("templates/shared/*"))
-}
+type ArticleController struct{}
 
 func (a ArticleController) Index(db *sql.DB) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    articles, err := ArticleModel{}.RetrieveAll(db) 
-    if err != nil {
-      log.Fatal(err)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		articles, err := ArticleModel{}.RetrieveAll(db)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    data := struct {
-      Title    string
-      Articles []ArticleModel
-    }{
-      "Articles",
-      articles,
-    }
+		data := struct {
+			Title    string
+			Articles []ArticleModel
+		}{
+			"Articles",
+			articles,
+		}
 
-    t.ExecuteTemplate(w, "index", data) 
-  }
+		err = t.ExecuteTemplate(w, "articleIndex", data)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
 
 func (a ArticleController) Retrieve(db *sql.DB) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    article, err := ArticleModel{}.RetrieveOne(db, r.FormValue("q")) 
-    if err != nil {
-      log.Fatal(err)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		article, err := ArticleModel{}.RetrieveOne(db, r.FormValue("q"))
+		if err != nil {
+			log.Fatal(err)
+		}
 
-    data := struct {
-      Title    string
-      Article   ArticleModel
-    }{
-      article.Title,
-      article,
-    }
+		data := struct {
+			Title   string
+			Article ArticleModel
+		}{
+			article.Title,
+			article,
+		}
 
-    t.ExecuteTemplate(w, "show", data) 
-  }
+		t.ExecuteTemplate(w, "articleShow", data)
+	}
 }
 
 func (a ArticleController) Form(db *sql.DB) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    t.ExecuteTemplate(w, "form", nil) 
-  }
+	return func(w http.ResponseWriter, r *http.Request) {
+		t.ExecuteTemplate(w, "form", nil)
+	}
 }
-
