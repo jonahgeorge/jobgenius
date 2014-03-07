20edc8a59d1747f784ffb50f5f0b8aa62f3e3209
jonahgeorge <jonah.george@icloud.com>
2014-03-07 09:32:14 -0800

    Added landing page

--- controllers/static.go
+++ controllers/static.go
@@ -1,11 +1,46 @@
 package controllers
 
 import (
+	"database/sql"
+	_ "github.com/Go-SQL-Driver/MySQL"
+	. "github.com/jonahgeorge/husker/models"
 	"log"
 	"net/http"
 )
 
 type StaticController struct{}
+
+func (s StaticController) Landing(db *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		users, err := UserModel{}.RetrieveAll(db)
+		articles, err := ArticleModel{}.RetrieveAll(db)
+		interviews, err := InterviewModel{}.RetrieveAll(db)
+		if err != nil {
+			log.Fatal(err)
+		}
+		signedIn := false
+
+		data := struct {
+			Title      string
+			Users      []UserModel
+			Articles   []ArticleModel
+			Interviews []InterviewModel
+		}{
+			"Welcome",
+			users,
+			articles,
+			interviews,
+		}
+
+		if signedIn == false {
+			err := t.ExecuteTemplate(w, "landingTemplate", data)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
 
 func (s StaticController) About() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
