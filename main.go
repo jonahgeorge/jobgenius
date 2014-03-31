671c94410f13ac3958438bb227d2b750f6d00dcf
jonahgeorge <jonah.george@icloud.com>
2014-03-31 00:12:11 -0700

    Switching to gorilla muxer and moving port value to config file

--- main.go
+++ main.go
@@ -3,26 +3,23 @@ package main
 import (
 	"database/sql"
 	_ "github.com/Go-SQL-Driver/MySQL"
-	"github.com/gorilla/pat"
+	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
 	"github.com/gosexy/to"
 	"github.com/gosexy/yaml"
-	. "github.com/jonahgeorge/jobgenius.net/controllers"
+	"github.com/jonahgeorge/jobgenius.net/controllers"
 	"log"
 	"net/http"
 )
 
-var db *sql.DB
-var store *sessions.CookieStore
-var user, pass, name, secret string
-
 func main() {
 	// load settings from config file
 	conf, _ := yaml.Open("settings.yml")
-	user = to.String(conf.Get("database", "user"))
-	pass = to.String(conf.Get("database", "pass"))
-	name = to.String(conf.Get("database", "name"))
-	secret = to.String(conf.Get("session", "secret"))
+	user := to.String(conf.Get("database", "user"))
+	pass := to.String(conf.Get("database", "pass"))
+	name := to.String(conf.Get("database", "name"))
+	secret := to.String(conf.Get("session", "secret"))
+	port := to.String(conf.Get("port"))
 
 	// open database connection
 	db, err := sql.Open("mysql", user+":"+pass+"@/"+name)
@@ -33,38 +30,46 @@ func main() {
 	db.SetMaxIdleConns(100)
 
 	// initialize session storage
-	store = sessions.NewCookieStore([]byte(secret))
+	store := sessions.NewCookieStore([]byte(secret))
 
-	// intialize gorilla router
-	r := pat.New()
-	r.Get("/articles", ArticleController{}.Index(db, store))
-	r.Get("/article/{id:[0-9]+}", ArticleController{}.Retrieve(db, store))
-	r.Get("/article", ArticleController{}.Form(db, store))
-	r.Post("/article", ArticleController{}.Create(db, store))
+	// intialize routes muxer
+	r := mux.NewRouter()
 
-	r.Get("/interviews", InterviewController{}.Index(db, store))
-	r.Get("/interview/{id:[0-9]+}", InterviewController{}.Retrieve(db, store))
-	r.Get("/interview", InterviewController{}.Form(db, store))
+	// article routes
+	r.HandleFunc("/articles", controllers.Article{}.Index(db, store))
+	r.HandleFunc("/article/{id:[0-9]+}", controllers.Article{}.Retrieve(db, store))
+	r.HandleFunc("/article", controllers.Article{}.Form(db, store)).Methods("GET")
+	r.HandleFunc("/article", controllers.Article{}.Create(db, store)).Methods("POST")
 
-	r.Get("/accounts", AccountController{}.Index(db, store))
-	r.Get("/account/{id:[0-9]+}", AccountController{}.Retrieve(db, store))
+	// interview routes
+	r.HandleFunc("/interviews", controllers.Interview{}.Index(db, store))
+	r.HandleFunc("/interview/{id:[0-9]+}", controllers.Interview{}.Retrieve(db, store))
+	r.HandleFunc("/interview", controllers.Interview{}.Form(db, store))
 
-	r.Get("/about", StaticController{}.About(store))
-	r.Get("/terms", StaticController{}.Terms(store))
-	r.Get("/privacy", StaticController{}.Privacy(store))
+	// account routes
+	r.HandleFunc("/accounts", controllers.Account{}.Index(db, store))
+	r.HandleFunc("/account/{id:[0-9]+}", controllers.Account{}.Retrieve(db, store))
 
-	r.Get("/signin", UserController{}.SignInForm(store))
-	r.Get("/signout", UserController{}.SignOut(store))
-	r.Get("/signup", UserController{}.SignUpForm(store))
+	// static page routes
+	r.HandleFunc("/about", controllers.Static{}.About(store))
+	r.HandleFunc("/terms", controllers.Static{}.Terms(store))
+	r.HandleFunc("/privacy", controllers.Static{}.Privacy(store))
 
-	r.Post("/api/signin", UserController{}.SignInApi(db, store))
-	r.Post("/api/signup", UserController{}.SignUpApi(db, store))
+	// user routes
+	r.HandleFunc("/signin", controllers.User{}.SignInForm(store))
+	r.HandleFunc("/signout", controllers.User{}.SignOut(store))
+	r.HandleFunc("/signup", controllers.User{}.SignUpForm(store))
 
+	// api routes
+	r.HandleFunc("/api/signin", controllers.User{}.SignInApi(db, store))
+	r.HandleFunc("/api/signup", controllers.User{}.SignUpApi(db, store))
+
+	// static resource files
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
-	r.Get("/", StaticController{}.Landing(db, store))
 
-	// have our app use gorilla router
+	// route path
+	r.HandleFunc("/", controllers.Static{}.Landing(db, store))
+
 	http.Handle("/", r)
-
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":"+port, nil)
 }
