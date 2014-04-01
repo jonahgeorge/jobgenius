7310480a9fcc7256a9bae49a69221f9f5f10b380
jonahgeorge <jonah.george@icloud.com>
2014-04-01 13:21:38 -0700

    Fixed formatting of Industry and Environment fields

--- models/blocks/basic.go
+++ models/blocks/basic.go
@@ -9,9 +9,9 @@ import (
 type BasicBlock struct {
 	Type            sql.NullString
 	Sector          sql.NullString
-	Industry        sql.NullString
+	Industry        []Field
 	Experience      sql.NullString
-	Environment     sql.NullString
+	Environment     []Field
 	Salary          sql.NullString
 	HoursPerWeek    sql.NullString
 	WeekendsWorked  sql.NullString
@@ -23,15 +23,15 @@ func (b BasicBlock) RetrieveById(db *sql.DB, id string) BasicBlock {
 	sql := `SELECT
     			L_TYPE.value,
     			L_SECTOR.value,
-    			Industry.value,
     			L_EXPERIENCE.value,
-    			Environment.value,
     			L_SALARY.value,
     			L_HOURS_PER_WEEK.value,
     			L_WEEKENDS_WORKED.value,
     			L_OVERNIGHT_TRAVEL.value
+
     		FROM
     			C_INTERVIEW
+
             LEFT JOIN 
             	F_TYPE ON F_TYPE.iid = C_INTERVIEW.id
             LEFT JOIN 
@@ -42,24 +42,10 @@ func (b BasicBlock) RetrieveById(db *sql.DB, id string) BasicBlock {
             LEFT JOIN 
             	L_SECTOR ON L_SECTOR.id = F_SECTOR.vid
             
-            LEFT JOIN (
-              select F_INDUSTRY.iid, group_concat(L_INDUSTRY.value separator ', ') as value
-              from F_INDUSTRY
-              LEFT JOIN L_INDUSTRY ON L_INDUSTRY.id = F_INDUSTRY.vid
-              group by F_INDUSTRY.iid
-            ) as industry ON industry.iid = C_INTERVIEW.id
-            
             LEFT JOIN 
             	F_EXPERIENCE ON F_EXPERIENCE.iid = C_INTERVIEW.id
             LEFT JOIN 
             	L_EXPERIENCE ON L_EXPERIENCE.id = F_EXPERIENCE.vid
-            
-            LEFT JOIN (
-              select F_ENVIRONMENT.iid, group_concat(L_ENVIRONMENT.value separator ', ') as value
-              from F_ENVIRONMENT
-              LEFT JOIN L_ENVIRONMENT ON L_ENVIRONMENT.id = F_ENVIRONMENT.vid
-              group by F_ENVIRONMENT.iid
-            ) as environment ON environment.iid = C_INTERVIEW.id
             
             LEFT JOIN 
             	F_SALARY ON F_SALARY.iid = C_INTERVIEW.id
@@ -87,10 +73,79 @@ func (b BasicBlock) RetrieveById(db *sql.DB, id string) BasicBlock {
 	var block BasicBlock
 
 	row := db.QueryRow(sql, id)
-	err := row.Scan(&block.Type, &block.Sector, &block.Industry, &block.Experience, &block.Environment, &block.Salary, &block.HoursPerWeek, &block.WeekendsWorked, &block.OvernightTravel)
+	err := row.Scan(&block.Type, &block.Sector, &block.Experience, &block.Salary, &block.HoursPerWeek, &block.WeekendsWorked, &block.OvernightTravel)
 	if err != nil {
 		log.Printf("%s", err)
 	}
 
+	block.Industry = BasicBlock{}.RetrieveIndustry(db, id)
+	block.Environment = BasicBlock{}.RetrieveEnvironment(db, id)
+
 	return block
+}
+
+func (b BasicBlock) RetrieveIndustry(db *sql.DB, id string) []Field {
+
+	sql := `SELECT
+                L_INDUSTRY.id,
+                L_INDUSTRY.value
+            FROM
+                F_INDUSTRY
+            LEFT JOIN
+                L_INDUSTRY ON L_INDUSTRY.id = F_INDUSTRY.vid
+            WHERE
+                F_INDUSTRY.iid = ?`
+
+	var fields []Field
+
+	rows, err := db.Query(sql, id)
+	if err != nil {
+		log.Printf("%s", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var field Field
+
+		err = rows.Scan(&field.Key, &field.Value)
+		if err != nil {
+			log.Printf("%s", err)
+		}
+
+		fields = append(fields, field)
+	}
+	return fields
+}
+
+func (b BasicBlock) RetrieveEnvironment(db *sql.DB, id string) []Field {
+
+	sql := `SELECT
+                L_ENVIRONMENT.id,
+                L_ENVIRONMENT.value
+            FROM
+                F_ENVIRONMENT
+            LEFT JOIN
+                L_ENVIRONMENT ON L_ENVIRONMENT.id = F_ENVIRONMENT.vid
+            WHERE
+                F_ENVIRONMENT.iid = ?`
+
+	var fields []Field
+
+	rows, err := db.Query(sql, id)
+	if err != nil {
+		log.Printf("%s", err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var field Field
+
+		err = rows.Scan(&field.Key, &field.Value)
+		if err != nil {
+			log.Printf("%s", err)
+		}
+
+		fields = append(fields, field)
+	}
+	return fields
 }
